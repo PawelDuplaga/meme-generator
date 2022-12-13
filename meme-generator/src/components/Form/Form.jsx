d9b03d3bbc25babe472d43a09dafa5ae68b55ab8
PawelDuplaga <pawelduplagait@gmail.com>
2022-12-13 23:45:14 +0100

    small fixes

--- meme-generator/src/components/Form/Form.jsx
+++ meme-generator/src/components/Form/Form.jsx
@@ -1,52 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import './Form.css'
 import memeData from '../../data/memeDatas'
 
 
 export default function Form () {
 
-    let url
 
-    function handleClick(string) {
-        console.log({string});
-    }
-
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-      }
+    const[meme,setMeme] = React.useState({
+        topText:"",
+        bottomText:"",
+        randomImg:"http://i.imgflip.com/1bij.jpg"
+    })
+    const[allMemeImages,setAllMemeImages] = useState(memeData)
 
     function getRandomMemeData () {
-        const index = getRandomInt(0,memeData.data.memes.length)
-        const memeUrl = memes[index].url;
-        url = memeUrl;
-        console.log(index);
-        console.log(memeUrl);
+        const memesArray = allMemeImages.data.memes
+        const randomIndex = Math.floor(Math.random() * memesArray.length)
+        const memeUrl =  memesArray[randomIndex].url
 
-        return memes[index];
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImg: memeUrl
+        }))
     }
+
 
 
     return (
-        <main>
-            <div className="form-container">
-                <input
-                type="text"
-                placeholder="Top text"
-                className="form--input"
-                />
-                <input
-                type="text"
-                placeholder="Bottom text"
-                className="form--input"
-                />
-                <button 
-                onClick={getRandomMemeData}
-                className="form--button">
-                    Get a new meme image
-                </button>
-            </div>
-        </main>
+        <div>
+            <main>
+                <div className="form-container">
+                    <input
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    />
+                    <input
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    />
+                    <button 
+                    onClick={getRandomMemeData}
+                    className="form--button">
+                        Get a new meme image
+                    </button>
+                </div>
+            </main>
+            <img className="meme-image" src = {meme.randomImg}/>
+        </div>
     )
 }
